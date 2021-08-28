c45cb0366d96457f537f4be42195a04fb38b7d25
david.giczi@gmail.com <david.giczi@gmail.com>
2021-08-28 09:49:03 +0200

    Project is created

---
+++ src/main/java/com/david/giczi/webtris/WebtrisApplication.java
@@ -0,0 +1,13 @@
+package com.david.giczi.webtris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/david/giczi/webtris/controller/WebtrisOperations.java
@@ -0,0 +1,14 @@
+package com.david.giczi.webtris.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/webtris")
+public class WebtrisOperations {
+
+	@RequestMapping("")
+	public String goLoginPage() {
+		return "login";
+	}
+}
