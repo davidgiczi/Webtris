1960d68753e9cb84f14050ecfabd38f49b99fc9a
david.giczi@gmail.com <david.giczi@gmail.com>
2021-09-07 22:21:24 +0200

    Running on Heroku

--- src/main/java/com/david/giczi/webtris/repository/PlayerRepository.java
+++ src/main/java/com/david/giczi/webtris/repository/PlayerRepository.java
@@ -9,8 +9,8 @@ import com.david.giczi.webtris.model.Player;
 public interface PlayerRepository extends CrudRepository<Player, Long> {
 
 	
-	@Query(value = "select * from players where name like :name and birth_date like :birthdate", nativeQuery = true)
+	@Query(value = "select * from players where name like :name and birth_date = :birthdate", nativeQuery = true)
 	Player getPlayerByNameAndBirthDate(@Param("name") String name, @Param("birthdate") Date birthDate);
-	@Query(value = "select id from players where name like :name and birth_date like :birthdate", nativeQuery = true)
+	@Query(value = "select id from players where name like :name and birth_date = :birthdate", nativeQuery = true)
 	Long getPlayerIdByNameAndBirthDate(@Param("name") String name, @Param("birthdate") Date birthDate);
 }
